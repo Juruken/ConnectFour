f0c4cef426d245ee57bfa2131fcb58fa0b6164e4
Juruken <donte.wyrmfoe@gmail.com>
2016-07-12 19:24:17 +1000

    Add more to DiagonalGameVictoryValidator.

--- ConnectFour/Validators/DiagonalGameVictoryValidator.cs
+++ ConnectFour/Validators/DiagonalGameVictoryValidator.cs
@@ -9,7 +9,9 @@ namespace ConnectFour.Validators
         public bool Validate(GameGrid gameGrid, Players player)
         {
             return ValidateDownAndRight(gameGrid, player)
-                || ValidateDownAndLeft(gameGrid, player);
+                || ValidateUpAndRight(gameGrid, player)
+                || ValidateDownAndLeft(gameGrid, player)
+                || ValidateUpAndLeft(gameGrid, player);
         }
 
         /// <summary>
@@ -22,7 +24,6 @@ namespace ConnectFour.Validators
         {
             var grid = gameGrid.Grid;
 
-            // We want to ALWAYS do 3 at max?
             for (var i = 0; i < gameGrid.Rows - 3; i++)
             {
                 for (var j = 0; j < gameGrid.Columns - 3; j++)
@@ -31,6 +32,36 @@ namespace ConnectFour.Validators
                     var token2 = grid[i + 1][j + 1];
                     var token3 = grid[i + 2][j + 2];
                     var token4 = grid[i + 3][j + 3];
+
+                    if (token1 == player
+                        && token2 == player
+                        && token3 == player
+                        && token4 == player)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+        
+        /// <summary>
+        /// Check for diagonal win, going up and right (starting bottom left)
+        /// </summary>
+        /// <param name="gameGrid"></param>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        private bool ValidateUpAndRight(GameGrid gameGrid, Players player)
+        {
+            var grid = gameGrid.Grid;
+
+            for (var i = 0; i < gameGrid.Rows - 3; i++)
+            {
+                for (var j = gameGrid.Columns - 1; j > 2; j--)
+                {
+                    var token1 = grid[i][j];
+                    var token2 = grid[i + 1][j - 1];
+                    var token3 = grid[i + 2][j - 2];
+                    var token4 = grid[i + 3][j - 3];
 
                     if (token1 == player
                         && token2 == player
@@ -52,19 +83,18 @@ namespace ConnectFour.Validators
         private bool ValidateDownAndLeft(GameGrid gameGrid, Players player)
         {
             var grid = gameGrid.Grid;
-            
+
             // Start at the highest row
-            for (var i = gameGrid.Rows - 1 ; i > 3; i--)
+            for (var i = gameGrid.Rows - 1; i > 3; i--)
             {
                 // Why only 3?
-                // TODO: Figure out... how high to go..
-                for (var j = 0; j < gameGrid.Columns - 3; j++)
+                for (var j = 0; j < gameGrid.Columns - 4; j++)
                 {
                     var token1 = grid[i][j];
                     var token2 = grid[i - 1][j + 1];
                     var token3 = grid[i - 2][j + 2];
                     var token4 = grid[i - 3][j + 3];
-                    
+
                     if (token1 == player
                         && token2 == player
                         && token3 == player
@@ -77,34 +107,6 @@ namespace ConnectFour.Validators
         }
 
         /// <summary>
-        /// Check for diagonal win, going up and right (starting bottom left)
-        /// </summary>
-        /// <param name="gameGrid"></param>
-        /// <param name="player"></param>
-        /// <returns></returns>
-        private bool ValidateUpAndRight(GameGrid gameGrid, Players player)
-        {
-            var grid = gameGrid.Grid;
-            throw new System.NotImplementedException();
-
-            /*for (i = 0; i < 3; i++)
-            {
-                for (j = 6; j > 2; j--)
-                {
-                    var token1 = grid[i][j];
-                    var token2 = grid[i + 1][j - 1];
-                    var token3 = grid[i + 2][j - 2];
-                    var token4 = grid[i + 3][j - 3];
-
-                    if (token1 == player
-                        && token2 == player
-                        && token3 == player
-                        && token4 == player)
-                        return true;                }
-            }*/
-        }
-
-        /// <summary>
         /// Check for diagonal win, going up and left (starting bottom right)
         /// </summary>
         /// <param name="gameGrid"></param>
@@ -113,10 +115,10 @@ namespace ConnectFour.Validators
         private bool ValidateUpAndLeft(GameGrid gameGrid, Players player)
         {
             var grid = gameGrid.Grid;
-            throw new System.NotImplementedException();
-            /*for (var i = 5; i > 3; i--)
+
+            for (var i = gameGrid.Rows - 1; i > 3; i--)
             {
-                for (var j = 6; j > 2; j--)
+                for (var j = gameGrid.Columns - 1; j > 2; j--)
                 {
                     var token1 = grid[i][j];
                     var token2 = grid[i - 1][j - 1];
@@ -129,7 +131,9 @@ namespace ConnectFour.Validators
                         && token4 == player)
                         return true;
                 }
-            }*/
+            }
+            
+             return false;
         }
     }
 }
--- ConnectFourTests/Unit/Validators/DiagonalGameVictoryValidatorTests.cs
+++ ConnectFourTests/Unit/Validators/DiagonalGameVictoryValidatorTests.cs
@@ -15,19 +15,28 @@ namespace ConnectFourTests.Unit.Validators
             m_GameVictoryValidator = new DiagonalGameVictoryValidator();
         }
 
-        [Test, TestCaseSource(typeof(DiagonalDownAndRightVictoryTestDataProvider), "GetVictoryDetectedTestCases")]
+        [Test, TestCaseSource(typeof(DiagonalDownAndRightVictoryTestDataProvider), "GetUpAndRight")]
         public void TestVictoryDetectedDownAndRight(int rows, int columns, List<Tuple<int,int>> rowColumnToPopulate)
         {
             var gameGrid = CreateGameGrid(rows, columns, rowColumnToPopulate);
             var result = m_GameVictoryValidator.Validate(gameGrid, m_WinningPlayer);
             Assert.IsTrue(result);
         }
+
+        [Test, TestCaseSource(typeof(DiagonalDownAndRightVictoryTestDataProvider), "GetDownAndRight")]
+        public void TestVictoryDetectedDownAndLeft(int rows, int columns, List<Tuple<int, int>> rowColumnToPopulate)
+        {
+            var gameGrid = CreateGameGrid(rows, columns, rowColumnToPopulate);
+            var result = m_GameVictoryValidator.Validate(gameGrid, m_WinningPlayer);
+            Assert.IsTrue(result);
+        }
+
     }
 }
 
 public class DiagonalDownAndRightVictoryTestDataProvider
 {
-    public static IEnumerable GetVictoryDetectedTestCases
+    public static IEnumerable GetUpAndRight
     {
         get
         {
@@ -63,7 +72,13 @@ public class DiagonalDownAndRightVictoryTestDataProvider
                 new Tuple<int, int>(3, 2),
                 new Tuple<int, int>(4, 3)
             }).SetDescription("This should detect a victory in diagonally from 1, 0 to 4, 3");
+        }
+    }
 
+    public static IEnumerable GetDownAndRight
+    {
+        get
+        {
             // Down and Right
             yield return new TestCaseData(5, 5, new List<Tuple<int, int>>
             {
